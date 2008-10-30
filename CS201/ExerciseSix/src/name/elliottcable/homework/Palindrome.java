efc63f44faf799f0a56360730cce90a02ec2fc62
elliottcable <git@elliottcable.name>
2008-10-29 21:06:46 -0800

    Initial Palindrome class

---
+++ CS201/ExerciseSix/src/name/elliottcable/homework/Palindrome.java
@@ -0,0 +1,61 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+//	elliottcable
+// 	Assignment 6, Programming Project 3
+//	October 29, 2008
+//
+//////////////////////////////////////////////////////////////////////////////
+package name.elliottcable.homework;
+
+import java.util.Scanner;
+
+//////////////////////////////////////////////////////////////////////////////
+//
+// Description:   This class provides a way to store a string of characters,
+//                  and subsequently test if that string is (or is not) a
+//                  palindrome (i.e. reads the same backwards as forwards).
+//	
+// Inputs:        N/A
+//	
+// Outputs:       N/A
+//
+//////////////////////////////////////////////////////////////////////////////
+public class Palindrome {
+  
+  static Scanner keyboard = new Scanner(System.in);
+  
+  public static void main(String args[]) {
+    boolean enter_another;
+    do {
+      System.out.println("Enter a phrase, and I'll test whether or not it's a palindrome:");
+      char[] characters = keyboard.nextLine().toCharArray();
+
+      if( isPalindrome(characters, characters.length) ) {
+        System.out.print("That's a palindrome! ");
+      } else {
+        System.out.print("That's not a palindrome. ");
+      }
+      
+      System.out.println("Do you want to test another? (y/n)");
+      String response = keyboard.nextLine();
+      enter_another = (response.contains("y") || response.contains("Y"));
+    } while(enter_another);
+  }
+  
+  /**
+   * Name:          isPalindrome
+   * Description:   Checks whether characters are actually a palindrome or not.
+   * 
+   * Returns:       isPalindrome whether or not this Palindrome object's
+   *                  characters array is actually a palindrome.
+   */
+  public static boolean isPalindrome(char[] characters, int used) {
+    int middle = used / 2;
+    
+    for(int i = 0; i <= middle; i++){
+      if(characters[i] != characters[used - i - 1])
+        return false;
+    }
+    return true;
+  }
+}
