836234b84e1aa4ba2cd07c95bef719ebae327b7b
elliottcable <git@elliottcable.name>
2008-10-30 17:42:23 -0800

    Added a TicTacToe board.

--- CS201/ExerciseSix/Rakefile.rb
+++ CS201/ExerciseSix/Rakefile.rb
@@ -37,6 +37,39 @@ namespace :source do
   
 end
 
+# rake runner:{build,run}[Whatever]
+namespace :runner do
+  
+  desc 'compiles the Java runner into bytecode'
+  task :build, :identifier do |_, params|
+    needs_identifier! params[:identifier]
+    
+    bits = [:name, :elliottcable, :runners].map(&:to_s)
+    bits += params[:identifier].split '.'
+    
+    command = ['javac']
+    command << "-classpath #{Library.join ':'}" unless Library.empty?
+    command << "-sourcepath #{SourcePath}"
+    command << "-d #{BuildPath}"
+    command << "#{ File.join(SourcePath, bits) }Runner.java"
+    system command.join ' '
+  end
+  
+  desc 'runs the main() function'
+  task :run, :identifier do |_, params|
+    needs_identifier! params[:identifier]
+    
+    bits = [:name, :elliottcable, :runners].map(&:to_s)
+    bits += params[:identifier].split '.'
+    
+    command = ['java']
+    command << "-classpath #{ [BuildPath, Library].join ':' }"
+    command << "#{ bits.join('.') }Runner"
+    system command.join ' '
+  end
+  
+end
+
 def needs_identifier! arg
   raise 'you must pass an identifier as an argument' unless arg
 end
