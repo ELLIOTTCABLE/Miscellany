ea60336b132fce373734e270ee8016447a88cd1a
elliottcable <git@elliottcable.name>
2009-03-27 23:30:39 -0800

    Getting started on a subroutine to check the input buffer for new statements

--- CS221/Project3/validator.s
+++ CS221/Project3/validator.s
@@ -47,6 +47,59 @@
   
   li $a0, 1
   j ERROR
+  CHECK_INPUT_BUFFER:
+    li $t0, 0 # byte = null
+    li $t1, 0 # statement_complete = false
+    li $t2, 0 # reading_statement = false
+    move $t3, $a0 # i
+    move $t4, $a0 # j
+    move $t5, $a1 # k
+  
+    CHECK_INPUT_BUFFER_start:
+      lb $t0, $t3
+      beq $t0, 0, CHECK_INPUT_BUFFER_null
+      beq $t0, 52, CHECK_INPUT_BUFFER_semicolon
+      beq $t0, 32, CHECK_INPUT_BUFFER_space
+      bnez $t1, CHECK_INPUT_BUFFER_copy_up
+      j CHECK_INPUT_BUFFER_copy_down
+    
+    CHECK_INPUT_BUFFER_end:
+      addi $t3, 1
+      addi $t4, 1
+      j CHECK_INPUT_BUFFER_start
+    
+    CHECK_INPUT_BUFFER_increment:
+      addi $t5, 1
+      j CHECK_INPUT_BUFFER_start
+    
+    CHECK_INPUT_BUFFER_null:
+      sb $t0, $a1
+      sb $t0, $t4
+    
+    CHECK_INPUT_BUFFER_semicolon:
+      bnez $t1, CHECK_INPUT_BUFFER_copy_up
+      beqz $t2, CHECK_INPUT_BUFFER_end
+      li $t1, 1 # statement_complete = true
+      move $t4, $a0 # reset j
+      li $t0, 0 # copy down a null byte instead of a semicolon
+      j CHECK_INPUT_BUFFER_copy_down
+    
+    CHECK_INPUT_BUFFER_space:
+      bnez $t1, CHECK_INPUT_BUFFER_copy_up
+      bnez $t2, CHECK_INPUT_BUFFER_copy_down
+      j CHECK_INPUT_BUFFER_end
+    
+    CHECK_INPUT_BUFFER_copy_down:
+      li $t2, 1 # reading_statement = true
+      sb $t0, $a1
+      j CHECK_INPUT_BUFFER_end
+    
+    CHECK_INPUT_BUFFER_copy_up:
+      sb $t0, $t4
+      j CHECK_INPUT_BUFFER_end
+  
+  li $a0, 2
+  j ERROR
   EXIT:
     la $a0, Goodbye($zero)
     li $v0, 4; syscall # print_string
