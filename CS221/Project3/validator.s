f519aaf1c11448efc9ac1c3aa90f22232ca609af
elliottcable <git@elliottcable.name>
2009-03-27 19:27:00 -0800

    Trying something new: A global loop to do everything, checking for input and storing bits of input in buffers.

--- CS221/Project3/validator.s
+++ CS221/Project3/validator.s
@@ -1,103 +1,44 @@
-########################################################################
-#
-#   Elliott Cable <a221@elliottcable.name>
-#  Ken Kelley <askjk11@uaa.alaska.edu>
-#   Looping Example
-#   February 15th, 2009
-#   CS A221: Programming Assignment 2
-# 
-#   PURPOSE:  To loop through the instructions in this program 
-#    identifying each type of instruction used (R, I, J),
-#    which allows us to keep a count of the various types,
-#    essentially demonstrating the use of a program itself
-#    as its own data.
-# 
-#   ALGORITHM:
-#           RS = 0
-#               JS = 0
-#               IS = 0
-#               s0 = setAddress("main")
-#               s1 = 0
-#               s2 = 0
-#               s3 = 0
-#               
-#    loop body:
-#                  t0 = getInstruction(s0)
-#
-#      if (t0 == 0x3E00008) 
-#      {
-#                 break;
-#      }
-#                 t1 = shiftRight(t0, 26)  //obtain opcode
-#    
-#                 if t1 == 0
-#      {
-#      s1++;
-#      }
-#                 if ((t1 == 2)||(t1 == 3))
-#      {
-#      s2++;
-#      }
-#             else
-#      {
-#      s3++;
-#      }
-#      s0 = s0 + 4;
-#                 end of loop
-#                 s3++  //increment I for j $ra
-#                 RS = s1 //store counters to memory
-#                 JS = s2
-#                IS = s3
-#                 return
-# 
-#   INPUTS:     None (Program uses its own instructions as data)
-# 
-#   OUTPUTS:    Four constants (Rs, Js, and Is) which return the
-#    total count of each type of instruction.
-#
-#######################################################################
- 
-  .data
-Buffer:  .asciiz "            "                                        
-Greeting:  .asciiz "Enter command:"
-Stop:   .asciiz "exit;"    
-Goodbye: .asciiz "Good-Bye!"
-test: .asciiz "Test!\n"                              
-  .text                        
-main:                           
-    li $v0, 4                      
-  la $a0, Greeting                
-  syscall                          
-                              
-    li $v0, 8                             
-    la $a0, Buffer                        
-    li $a1, 20                        
-    syscall
+.data
+  Greeting:     .asciiz "Enter commands, and hit return to process:\n"
+  Prompt:       .asciiz ">> "
+  Goodbye:      .asciiz "Awwww...\n"
+  # Buffers
+  BInput:       .asciiz ""
+  BProcessing:  .asciiz ""
+  # Debugging
+  WTF:          .asciiz "WTF!\n"
+  YAY:          .asciiz "YAY!\n"
+  
+.text
+  main:
+    la $a0, Greeting($zero);
+    li $v0, 4; syscall # print_string
+  
+  MOTHERLOOP_start:
+    la $ra, MOTHERLOOP_have_input;
+    lb $t1, BInput($zero);
+    beq $t1, 0, READ_NEW_INPUT;
+  MOTHERLOOP_have_input:
+    j EXIT;
+
+# ---- ---- ! ---- ---- #
+  
+  uhmmmm:
+    la $a0, WTF($zero);
+    li $v0, 4; syscall # print_string
+  
+  READ_NEW_INPUT:
+    la $a0, Prompt($zero);
+    li $v0, 4; syscall # print_string
     
-    li $v0, 4
-    la $a0, Buffer
-    syscall
+    la $a0, BInput;
+    li $a1, 128;
+    li $v0, 8; syscall # read_string
     
-    li $t0, 0
-    
- exittest:     
-    
-    lb $t1, Buffer($t0)
-    lb $t2, Stop($t0)
-    bne $t1, $t2, main
-    addi $t0, $t0, 1
-    beq $t0, 5, END
-    
-    li $v0, 4                      
-    la $a0, test                
-    syscall    
-    
-    j exittest 
-                           
-END:
-    li $v0, 4
-    la $a0, Goodbye
-    syscall
-    
-    li $v0, 10
-    syscall 
+    j $ra
+  
+  EXIT:
+    la $a0, Goodbye($zero)
+    li $v0, 4; syscall; # print_string
+
+    li $v0, 10; syscall; # exit
