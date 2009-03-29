33d2491a3d08410820e27427a3de79935b874057
elliottcable <git@elliottcable.name>
2009-03-28 20:30:46 -0800

    Trying a shifting algorithm instead of that mess

--- CS221/Project3/validator.s
+++ CS221/Project3/validator.s
@@ -7,8 +7,8 @@
   # Buffers
   # These have to have a lot of whitespace to pre-allocate the (possibly)
   # necessary memory
-  BInput:       .asciiz "                                                                                                                               "
-  BProcessing:  .asciiz "                    "
+  BInput:      .asciiz "                                                                                                                               "
+  BProcessing: .asciiz "                                                                                                                               "
   # Debugging
   YAY:          .asciiz "YAY!\n"
   
@@ -18,6 +18,9 @@
     li $v0, 4; syscall # print_string
   
   MOTHERLOOP_start:
+    la $ra, MOTHERLOOP_after_shift
+    j SHIFT
+  MOTHERLOOP_after_shift:
     la $ra, MOTHERLOOP_have_input
     lb $t1, BInput($zero)
     beq $t1, 0, READ_NEW_INPUT
@@ -49,61 +52,56 @@
   
   li $a0, 1
   j ERROR
-  CHECK_INPUT_BUFFER:
-    li $t0, 0 # byte = null
-    li $t1, 0 # statement_complete = false
-    li $t2, 0 # reading_statement = false
-    move $t3, $a0 # i
-    move $t4, $a0 # j
-    move $t5, $a1 # k
+  SHIFT:
+    addi $sp, $sp, -4
+    sw $s0, 0($sp)
+    move $s0, $ra
+    
+    SHIFT_start:
+      la $ra, SHIFT_middle
+      li $a2, 59
+      j COPY_STRING
+      
+    SHIFT_middle:
+      addi $v0, 1
+      la $ra, SHIFT_end
+      move $a1, $a0
+      move $a0, $v0
+      li $a2, 0
+      j COPY_STRING
+      
+    SHIFT_end:
+      move $ra, $s0
+      lw $s0, 0($sp)
+      addi $sp, $sp, 4
+      j $ra
   
-    CHECK_INPUT_BUFFER_start:
-      lb $t0, $t3
-      beq $t0, 0, CHECK_INPUT_BUFFER_null
-      beq $t0, 52, CHECK_INPUT_BUFFER_semicolon
-      beq $t0, 32, CHECK_INPUT_BUFFER_space
-      bnez $t1, CHECK_INPUT_BUFFER_copy_up
-      j CHECK_INPUT_BUFFER_copy_down
+  li $a0, 3
+  j ERROR
+  COPY_STRING:
+    move $t1, $a0
+    move $t2, $a1
     
-    CHECK_INPUT_BUFFER_end:
-      addi $t3, 1
-      addi $t4, 1
-      j CHECK_INPUT_BUFFER_start
+    COPY_STRING_loop_start:
+      lb $t0, 0($t1)
+      beq $t0, 0, COPY_STRING_end
+      beq $t0, $a2, COPY_STRING_end
+      sb $t0, 0($t2)
     
-    CHECK_INPUT_BUFFER_increment:
-      addi $t5, 1
-      j CHECK_INPUT_BUFFER_start
+    COPY_STRING_loop_end:
+      addi $t1, 1
+      addi $t2, 1
+      j COPY_STRING_loop_start
     
-    CHECK_INPUT_BUFFER_null:
-      sb $t0, $a1
-      sb $t0, $t4
-    
-    CHECK_INPUT_BUFFER_semicolon:
-      bnez $t1, CHECK_INPUT_BUFFER_copy_up
-      beqz $t2, CHECK_INPUT_BUFFER_end
-      li $t1, 1 # statement_complete = true
-      move $t4, $a0 # reset j
-      li $t0, 0 # copy down a null byte instead of a semicolon
-      j CHECK_INPUT_BUFFER_copy_down
-    
-    CHECK_INPUT_BUFFER_space:
-      bnez $t1, CHECK_INPUT_BUFFER_copy_up
-      bnez $t2, CHECK_INPUT_BUFFER_copy_down
-      j CHECK_INPUT_BUFFER_end
-    
-    CHECK_INPUT_BUFFER_copy_down:
-      li $t2, 1 # reading_statement = true
-      sb $t0, $a1
-      j CHECK_INPUT_BUFFER_end
-    
-    CHECK_INPUT_BUFFER_copy_up:
-      sb $t0, $t4
-      j CHECK_INPUT_BUFFER_end
+    COPY_STRING_end:
+      move $v0, $t1
+      sb $zero, 0($t2)
+      j $ra
   
   li $a0, 2
   j ERROR
   EXIT:
     la $a0, Goodbye($zero)
     li $v0, 4; syscall # print_string
-
+    
     li $v0, 10; syscall # exit
